6caae3dee0f3a116216d9c7d1557597e683ee060
Takuya Numata <takuya.numata@e-gravity.co.jp>
2019-01-26 17:08:07 +0900

    test

---
+++ kingofTime/MainWindowViewModel.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace kingofTime
+{
+    public class MainWindowViewModel
+    {
+
+        public string text
+        {
+            get { return "text"; }
+        }
+    }
+}
