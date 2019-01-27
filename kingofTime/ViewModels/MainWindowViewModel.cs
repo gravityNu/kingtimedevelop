442c0248436c4f8f41f3053ac564b9da627966cd
Takuya Numata <takuya.numata@e-gravity.co.jp>
2019-01-27 10:28:37 +0900

    追加忘れ

---
+++ kingofTime/ViewModels/MainWindowViewModel.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using kingofTime.Models;
+using Prism.Mvvm;
+
+namespace kingofTime.ViewsModels
+{
+    public class MainWindowViewModel : BindableBase
+    {
+
+        public MainWindowViewModel()
+        {
+
+        }
+
+        private string m_html;
+
+        public string Html
+        {
+            set { SetProperty(ref m_html, value); }
+            get { return m_html; }
+        }
+
+        public void ReturnHTML(string url)
+        {
+            Html = Webscraping.GetHtml(url);
+        }
+
+
+    }
+}
