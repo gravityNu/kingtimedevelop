fa86691f62e4177634e086e09c7bc85c26b18bbe
Takuya Numata <takuya.numata@e-gravity.co.jp>
2019-01-27 09:27:02 +0900

    xamlstyler導入 html表示をバインドに変更

--- kingofTime/App.xaml.cs
+++ kingofTime/App.xaml.cs
@@ -6,6 +6,9 @@ using System.Linq;
 using System.Threading.Tasks;
 using System.Windows;
 using kingofTime.Views;
+using kingofTime.ViewsModels;
+using Prism.Mvvm;
+using Unity;
 
 namespace kingofTime
 {
@@ -15,14 +18,20 @@ namespace kingofTime
     public partial class App : Application
     {
 
-        [STAThread]
-        public static void Main()
+
+        // アプリで管理するコンテナ
+        private IUnityContainer Container { get; } = new UnityContainer();
+
+        private void Application_Startup(object sender, StartupEventArgs e)
         {
-            var app = new App();
-            app.InitializeComponent();
-            var main = new MainWindowView();
-            app.Run(main);
+            ViewModelLocationProvider.SetDefaultViewModelFactory(x => this.Container.Resolve(x));
+            var vm = new MainWindowViewModel();
+            var main = this.Container.Resolve<MainWindowView>();
+            main.DataContext = vm;
+            main.Show();
+
         }
+
 
     }
 }
