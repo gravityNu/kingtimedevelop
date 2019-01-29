f47964099329be1a5b989811a65d371a3b8f9e1b
selciaaa <selciaaa@outlook.com>
2019-01-29 23:50:01 +0900

    ファイル追加

---
+++ kingofTime/Models/ReadDocs/PDFReader.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace kingofTime.Models.ReadDocs
+{
+    public class PDFReader
+    {
+        public int Read(string FileName)
+        {
+            return 0;
+        }
+    }
+}
---
+++ kingofTime/Models/ReadDocs/XPSReader.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Xps.Packaging;
+
+namespace kingofTime.Models.ReadDocs
+{
+    public class XPSReader
+    {
+        public int Read(string FileName)
+        {
+            System.Windows.Xps.Packaging.XpsDocument doc =
+                new System.Windows.Xps.Packaging.XpsDocument(FileName, System.IO.FileAccess.Read);
+            return 0;
+        }
+    }
+}
